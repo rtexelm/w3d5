215066dbb2ce37987c1022d26aaf357c1ee4c5ca
Anthony_Chiodi <adc8wlc@gmail.com>
2022-09-30 15:24:02 -0400

    Added the add_child and remove_child methods

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -9,16 +9,31 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        if parent == nil
-            @parent = node 
+        if node == nil
+            @parent = nil
+        elsif @parent == nil
+            @parent = node
             @parent.children << self
         else
             @parent.children.delete(self)
             @parent = node #if parent.nil?
             @parent.children << self
         end
-        
+
     end
+
+    def add_child(child_node) #"A" = "B"
+        # call #parent=
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "Not my kid!" unless @children.include?(child_node)
+        child_node.parent = nil
+    end
+
+    
+
 
 
 end
