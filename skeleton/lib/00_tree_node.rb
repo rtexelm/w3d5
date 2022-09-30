e38888dae98723bd33bdd503b8fcc6bf3ff9d2c2
Anthony_Chiodi <adc8wlc@gmail.com>
2022-09-30 14:32:24 -0400

    Added the parent = methdod for the PolyTreeNode problem

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,12 +1,20 @@
 class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
     def initialize(value)
         @parent = nil
-        @children = []
-        @value = value
+        @children = [] # [b, c, d]
+        @value = value # "A"
     end
 
-    def parent=(parent)
-        @parent = parent
+    def parent=(node)
+        # if parent == nil
+        @parent = node
         @parent.children << self
+        # if node != @parent do @parent.children.pop
+        # 
     end
+
+
 end
