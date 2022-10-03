8cfdac38dc44fa06fd380afcd265f5304aadf261
rtexelm <texelist@gmail.com>
2022-10-03 15:39:52 -0400

    Completed valid_moves method

--- knight_travails.rb
+++ knight_travails.rb
@@ -5,12 +5,21 @@ class KnightPathFinder
     def self.valid_moves(pos)
         row, col = pos
 
+        vld_moves = []
+
+        potential_moves = [[1,2], [1,-2], [-1, 2], [-1, -2], [2, 1], [-2, 1], [-2, -1], [2, -1]]
+
+        potential_moves.each do |sub|
+            x, y = sub
+            next_r = row + x
+            next_c = col + y
+            vld_moves << [next_r, next_c] if (next_r > 0 && next_c > 0) && (next_r < 7 && next_c < 7)
+        end
+
+        vld_moves
         # unless !(row > 7 || col > 7 ) && !(row < 0 || col < 0)
         #     raise "Invalid position"
         # end
-
-
-
     end
 
     def initialize(position)
@@ -19,10 +28,8 @@ class KnightPathFinder
     end
 
     def new_move_positions(pos)
-        KnightPathFinder.valid_moves(pos)
+        # KnightPathFinder.valid_moves(pos)
 
     end
-
-
 
 end
